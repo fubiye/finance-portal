8cbde54d3a9ab842da9193d39a5ea67d8a6dded2
Fu, Biye <biye.fu@outlook.com>
2019-09-10 22:15:42 +0800

    add domain module

---
+++ domain/src/main/java/com/biye/finance/domain/Hello.java
@@ -0,0 +1,12 @@
+package com.biye.finance.domain;
+
+public class Hello{
+    private String message = "Hello Domain";
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+}
