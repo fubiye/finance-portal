087046ae7f891792c045af891ca493fe33b054f9
Fu, Biye <biye.fu@outlook.com>
2019-09-10 23:37:13 +0800

    add modules

---
+++ ebidding/src/main/java/com/biye/finance/ebidding/BidWantedInCompetition.java
@@ -0,0 +1,10 @@
+package com.biye.finance.ebidding;
+
+/**
+ * BWIC - Bid wanted in competition
+ * @see https://www.investopedia.com/terms/b/bwic.asp
+ *
+ */
+
+public class BidWantedInCompetition {
+}
---
+++ ebidding/src/main/java/com/biye/finance/ebidding/conf/TestConfig.java
@@ -0,0 +1,7 @@
+package com.biye.finance.ebidding.conf;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestConfig {
+}
